6350e901b31a3d32e979422db4416be46654e178
Ivan Garcia <ival69@hotmail.com>
2025-08-08 13:39:54 +0200

    feat 3: usecases interface

---
+++ src/interfaces/NewsUseCase.ts
@@ -0,0 +1,9 @@
+import { News } from "../models/News";
+
+export interface NewsUseCases {
+  create(title: string, description: string, content: string, author: string): Promise<News>;
+  archive(newsId: string): Promise<void>;
+  getAllNews(): Promise<News[]>;
+  getAllArchived(): Promise<News[]>;
+  delete(newsId: string): Promise<void>;
+}
